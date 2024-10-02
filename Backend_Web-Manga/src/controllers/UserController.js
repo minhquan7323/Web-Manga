0d393aa3f26a55c2b94ba8b8a9ad07934eb8614b
minhquan7323 <minhquany9@gmail.com>
2024-10-02 14:23:31 +0700

    api sign up user

--- Backend_Web-Manga/src/controllers/UserController.js
+++ Backend_Web-Manga/src/controllers/UserController.js
@@ -2,9 +2,30 @@ const UserService = require('../services/UserService')
 
 const createUser = async (req, res) => {
     try {
-        console.log(req.body);
-        const res = await UserService.createUser()
-        return res.status(200).json(res)
+        const { name, email, password, confirmPassword, phone } = req.body
+        const reg = /^\w+([-+.'']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+        const isCheckEmail = reg.test(email)
+
+        if (!name || !email || !password || !confirmPassword || !phone) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The input is require'
+            })
+        }
+        else if (!isCheckEmail) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'Email is incorrect'
+            })
+        }
+        else if (password !== confirmPassword) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'Confirm password is incorrect'
+            })
+        }
+        const response = await UserService.createUser(req.body)
+        return res.status(200).json(response)
     } catch (e) {
         return res.status(404).json({
             message: e
