92ad6eb83ad55093280e0fcdb2a10c08e2296a2f
minhquan7323 <minhquany9@gmail.com>
2024-10-03 22:08:06 +0700

    refresh token

--- Backend_Web-Manga/src/routes/UserRouter.js
+++ Backend_Web-Manga/src/routes/UserRouter.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/UserController')
-const { authMiddleware } = require('../middleware/authMiddleware')
+const { authMiddleware, authUserMiddleware } = require('../middleware/authMiddleware')
 
 router.post('/signup', userController.createUser)
 router.post('/signin', userController.loginUser)
 router.put('/updateuser/:id', userController.updateUser)
 router.delete('/deleteuser/:id', authMiddleware, userController.deleteUser)
 router.get('/getalluser', authMiddleware, userController.getAllUser)
-router.get('/getdetailsuser/:id', authMiddleware, userController.getDetailsUser)
+router.get('/getdetailsuser/:id', authUserMiddleware, userController.getDetailsUser)
+router.post('/refreshtoken', userController.refreshTokenJwtService)
 
 module.exports = router
--- Backend_Web-Manga/src/services/JwtService.js
+++ Backend_Web-Manga/src/services/JwtService.js
@@ -5,16 +5,45 @@ dotenv.config()
 const genneralAccessToken = (payload) => {
     const access_token = jwt.sign({
         payload
-    }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+    }, process.env.ACCESS_TOKEN, { expiresIn: '30s' })
     return access_token
 }
+
 const genneralRefreshToken = (payload) => {
     const refresh_token = jwt.sign({
         payload
     }, process.env.REFRESH_TOKEN, { expiresIn: '365d' })
     return refresh_token
 }
+
+const refreshTokenJwtService = (token) => {
+    return new Promise((resolve, reject) => {
+        try {
+            jwt.verify(token, process.env.REFRESH_TOKEN, async (err, user) => {
+                if (err) {
+                    resolve({
+                        status: 'ERR',
+                        message: 'The authenication'
+                    })
+                }
+                const { payload } = user
+                const access_token = await genneralAccessToken({
+                    id: payload?.id,
+                    isAdmin: payload?.isAdmin
+                })
+                resolve({
+                    status: 'OK',
+                    message: 'Success',
+                    access_token: access_token
+                })
+            })
+        } catch (e) {
+            reject(e)
+        }
+    });
+}
 module.exports = {
     genneralAccessToken,
-    genneralRefreshToken
+    genneralRefreshToken,
+    refreshTokenJwtService
 }
