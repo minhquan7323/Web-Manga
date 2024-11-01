013db5470716492843b952898bca0da1aff6ca34
minhquan7323 <minhquany9@gmail.com>
2024-11-01 21:13:39 +0700

    ok

--- Backend_Web-Manga/src/index.js
+++ Backend_Web-Manga/src/index.js
@@ -9,10 +9,10 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 3001
 
-// app.use(cors({
-//     origin: 'https://backend-web-manga.onrender.com',
-//     credentials: true,
-// }))
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+}))
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(cookieParser())
