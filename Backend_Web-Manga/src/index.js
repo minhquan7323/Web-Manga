3b00bb0a5b56f333f39d92d488ff2d9d9c20e8ee
minhquan7323 <minhquany9@gmail.com>
2024-11-01 21:15:02 +0700

    ok

--- Backend_Web-Manga/src/index.js
+++ Backend_Web-Manga/src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { mongoose } = require('mongoose')
 const dotenv = require('dotenv')
 const routes = require('./routes')
-// const cors = require('cors')
+const cors = require('cors')
 const cookieParser = require('cookie-parser')
 dotenv.config()
 
