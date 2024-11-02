b39ee66d0b74f6ce8e7b255d8aada68c70b0bb6a
minhquan7323 <minhquany9@gmail.com>
2024-11-02 21:44:51 +0700

    ok

--- Backend_Web-Manga/src/index.js
+++ Backend_Web-Manga/src/index.js
@@ -18,7 +18,7 @@ app.use(cors())
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(cookieParser())
-app.use(express.static('public'))
+// app.use(express.static('public'))
 routes(app)
 mongoose.connect(`${process.env.MONGO_DB}`)
     .then(() => {
