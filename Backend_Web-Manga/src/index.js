541b835d939580dc323ae3246663568579ed1a67
minhquan7323 <minhquany9@gmail.com>
2024-11-02 21:59:19 +0700

    ok

--- Backend_Web-Manga/src/index.js
+++ Backend_Web-Manga/src/index.js
@@ -9,25 +9,15 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 3001
 
-// app.use(cors({
-//     origin: 'https://frontendweb-manga-git-main-minhquan7323s-projects.vercel.app',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     credentials: true
-// }))
-// app.use(cors())
 app.use(cors({
-    origin: (origin, callback) => {
-        callback(null, true); // Cho phép tất cả các nguồn
-    },
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'] // Đảm bảo các headers cần thiết
+    credentials: true
 }));
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(cookieParser())
-app.use('/static', express.static('public'));
-// app.use(express.static('public'))
+app.use(express.static('public'))
 routes(app)
 mongoose.connect(`${process.env.MONGO_DB}`)
     .then(() => {
