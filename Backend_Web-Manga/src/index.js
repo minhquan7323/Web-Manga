2407cbe169a977608e4c35321122bb4a769e6e9f
minhquan7323 <minhquany9@gmail.com>
2024-11-02 20:25:09 +0700

    ok

--- Backend_Web-Manga/src/index.js
+++ Backend_Web-Manga/src/index.js
@@ -6,8 +6,7 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 dotenv.config()
 
-// const app = express()
-app.use(express.static('public'))
+const app = express()
 const port = process.env.PORT || 3001
 
 app.use(cors({
@@ -18,6 +17,7 @@ app.use(cors({
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(cookieParser())
+app.use(express.static('public'))
 routes(app)
 mongoose.connect(`${process.env.MONGO_DB}`)
     .then(() => {
