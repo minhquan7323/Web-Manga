96842a30e88eb27685e7c5439a3a68272a597b88
minhquan7323 <minhquany9@gmail.com>
2024-11-02 21:00:11 +0700

    ok

--- Backend_Web-Manga/src/index.js
+++ Backend_Web-Manga/src/index.js
@@ -9,11 +9,12 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 3001
 
-app.use(cors({
-    origin: 'https://frontendweb-manga-git-main-minhquan7323s-projects.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-}))
+// app.use(cors({
+//     origin: 'https://frontendweb-manga-git-main-minhquan7323s-projects.vercel.app',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true
+// }))
+app.use(cors())
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(cookieParser())
