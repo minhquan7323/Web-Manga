78c58eb89cc86d8b4fcec67fede956e33e01666b
minhquan7323 <minhquany9@gmail.com>
2026-06-23 17:47:32 +0700

    add data

--- Backend_Web-Manga/src/middleware/authMiddleware.js
+++ Backend_Web-Manga/src/middleware/authMiddleware.js
@@ -3,29 +3,6 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const authMiddleware = (req, res, next) => {
-    const token = req.headers.token.split(' ')[1]
-
-    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, user) {
-        if (err) {
-            return res.status(404).json({
-                message: 'The authentication',
-                status: 'ERROR'
-            })
-        }
-        if (user?.isAdmin) {
-            next()
-        }
-        else {
-            return res.status(404).json({
-                message: 'The authentication',
-                status: 'ERROR'
-            })
-        }
-
-    })
-}
-
-const authMiddleware = (req, res, next) => {
     const tokenHeader = req.headers.token;
     if (!tokenHeader || !tokenHeader.startsWith('Bearer ')) {
         return res.status(401).json({
@@ -48,6 +25,31 @@ const authMiddleware = (req, res, next) => {
         } else {
             return res.status(403).json({
                 message: 'Bạn không có quyền Admin để thực hiện hành động này',
+                status: 'ERROR'
+            })
+        }
+    })
+}
+
+const authUserMiddleware = (req, res, next) => {
+    const token = req.headers.token.split(' ')[1]
+    const userId = req.params.id
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, user) {
+        if (err) {
+            return res.status(404).json({
+                message: 'The authentication',
+                status: 'ERROR'
+            })
+        }
+
+        // if (user?.isAdmin || user?.id === userId) {
+        if (user?.isAdmin || user?.id) {
+            next()
+        }
+        else {
+            return res.status(404).json({
+                message: 'The authentication',
                 status: 'ERROR'
             })
         }
